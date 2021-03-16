68030538cda6e7955a32fecdf75ca7672b7116b2
vjayaman <vjayaman314@gmail.com>
2021-03-16 09:15:05 -0500

    Merge file almost completely up to date (and now pretty minimal), still need to add geo and temp cluster average columns

--- merge_data.R
+++ merge_data.R
@@ -2,12 +2,16 @@ x <- c("magrittr", "tibble", "dplyr", "readxl", "purrr", "testit", "readr")
 y <- lapply(x, require, character.only = TRUE)
 assert("All packages loaded correctly", all(unlist(y)))
 
-repNA <- function(vec_x) {
-  ifelse(is.na(vec_x), 1.000, vec_x)
+repNA <- function(vec_x, i) {
+  ifelse(is.na(vec_x), i, vec_x)
+}
+
+writeData <- function(fp, df) {
+  write.table(df, fp, row.names = FALSE, quote = FALSE, sep = "\t")
 }
 
 # ------------------------------------------------------------------------------------------------------------
-# NOW SAVING OUTPUTS AND MERGING WITH CGM DATA ---------------------------------------------------------------
+# NOW SAVING OUTPUTS AND MERGING ECCS WITH CGM DATA ----------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------
 eccs <- read.table("results/ECCs.tsv", stringsAsFactors = FALSE, header = TRUE) %>% as_tibble()
 cgms <- read.table("results/CGM_strain_results.txt", sep = "\t", 
@@ -18,32 +22,42 @@ strain_data <- read_tsv("inputs/strain_data.tsv") %>%
   filter(TP2 == 1) # actually assigned a cluster at TP2, not NA (185 such cases)
 # ------------------------------------------------------------------------------------------------------------
 
-# strain_results <- left_join(step1, cem_st_results, by = "Strain") %>%
-  # select(Strain, Country, Province, City, Latitude, Longitude, Day, Month, Year,
-  #        TP1, first_tp1_flag, tp1_cl_size, TP1_ECC_0.0.1, TP1_ECC_0.1.0, 
-  #        TP2, first_tp2_flag, tp2_cl_size, TP2_ECC_0.0.1,
-  #        TP2_ECC_0.1.0, last_tp1_flag, last_tp2_flag, actual_size_change, add_tp1, num_novs,
-  #        actual_growth, novel_growth) %>%
-  # rename(TP1_cluster = first_tp1_flag, TP2_cluster = first_tp2_flag) %>%
-  # mutate(first_tp1_flag = TP1_cluster, first_tp2_flag = TP2_cluster,
-  #   tp1_cluster_size = tp1_cl_size, tp2_cluster_size = tp2_cl_size)
+step1 <- left_join(strain_data, cgms, by = "Strain") %>% 
+  left_join(., eccs, by = c("Strain", "TP1", "TP2"))
+step1[,grep("ECC", colnames(step1))] %<>% apply(., 2, repNA, i = 1.000) %>% as_tibble()
+step1[,grep("tp1_id", colnames(step1))] %<>% apply(., 2, repNA, i = "") %>% as_tibble()
 
-# strain_results[,c(13,14,18,19)] %<>% apply(., 2, repNA) %>% as_tibble()
-alldata <- left_join(strain_data, cgms) %>% left_join(., eccs)
-step1 <- alldata %>% 
+step2 <- step1 %>% rename("TP1 cluster" = tp1_id) %>% 
+  mutate("TP2 cluster" = first_tp2_flag, "TP1 cluster size" = tp1_cl_size, 
+         "TP2 cluster size" = tp2_cl_size) %>% 
   select(Strain, Country, Province, City, Latitude, Longitude, Day, Month, Year, 
-         TP1, tp1_id, tp1_cl_size, TP1_T0_ECC_0.0.1, TP1_T0_ECC_0.1.0, TP2, first_tp2_flag, tp2_cl_size, 
-         TP2_T0_ECC_0.0.1, TP2_T0_ECC_0.1.0, first_tp1_flag, last_tp1_flag, last_tp2_flag, tp1_cl_size, 
-         tp2_cl_size, actual_size_change, add_TP1, num_novs, actual_growth_rate, new_growth)
+         TP1, `TP1 cluster`, tp1_cl_size, TP1_T0_ECC_0.0.1, TP1_T0_ECC_0.1.0, 
+         TP2, `TP2 cluster`, tp2_cl_size, TP2_T0_ECC_0.0.1, TP2_T0_ECC_0.1.0,
+         first_tp1_flag, last_tp1_flag, first_tp2_flag, last_tp2_flag, `TP1 cluster size`, 
+         `TP2 cluster size`, actual_size_change, add_TP1, num_novs, actual_growth_rate, new_growth) %>% 
+  rename("TP1 cluster size (2)" = tp1_cl_size, 
+         "TP2 cluster size (2)" = tp2_cl_size, 
+         "First time this cluster was seen in TP1" = first_tp1_flag, 
+         "Last time this cluster was seen in TP1" = last_tp1_flag, 
+         "First time this cluster was seen in TP2" = first_tp2_flag, 
+         "Last time this cluster was seen in TP2" = last_tp2_flag, 
+         "Actual cluster size change (TP2 size - TP1 size)" = actual_size_change, 
+         "Number of additional TP1 strains in the TP2 match" = add_TP1, 
+         "Number of novels in the TP2 match" = num_novs, 
+         "Actual growth rate = (TP2 size - TP1 size) / (TP1 size)" = actual_growth_rate, 
+         "Novel growth = (TP2 size) / (TP2 size - number of novels)" = new_growth)
 
-# strain_results %>% set_colnames(c(
-#   colnames(strain_results)[1:10], "TP1 cluster", "TP1 cluster size", colnames(strain_results)[13:15],
-#   "TP2 cluster", "TP2 cluster size", colnames(strain_results)[18:19], "First time this cluster was seen in TP1",
-#   "Last time this cluster was seen in TP1", "First time this cluster was seen in TP2",
-#   "Last time this cluster was seen in TP2", "TP1 cluster size", "TP2 cluster size",
-#   "Actual cluster size change (TP2 size - TP1 size)", "Number of additional TP1 strains in TP2 match",
-#   "Number of novels in TP2 match", "Actual growth = (TP2 size - TP1 size) / (TP1 size)",
-#   "Novel growth = (TP2 size) / (TP2 size - number of novels)"
-# )) %>%
-#   write.table(., "processed_results/strain_data.txt", row.names = FALSE, quote = FALSE, sep = "\t")
+writeData(fp = "results/Strain_results.tsv", df = step2)
 
+
+step2 %>% arrange(`TP2 cluster`) %>% group_by(`TP2 cluster`) %>% slice(1) %>% 
+  select(-Strain) %>% ungroup() %>% 
+  writeData(fp = "results/Cluster_results.tsv", df = .)
+
+
+# We realized that the easiest way to get actual numbers for the temporal window and the geographic radius 
+# was to use the equations themselves. 
+# And it turns out that the first steps in the calculations are to generate temporal and geo matrices already. 
+# And then the data in these matrices is used to generate the ECC matrix
+# So we figure we may as well also report the cluster averages for geo and temporal
+# So we don't have to go back to a calibration curve to figure it out later
