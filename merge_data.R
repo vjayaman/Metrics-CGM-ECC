69a468092002fbd9e59cd50ddb91778bbcfe1b85
vjayaman <vjayaman314@gmail.com>
2021-03-26 14:31:28 -0500

    Commented out 'average' columns for now, until they're refined

--- merge_data.R
+++ merge_data.R
@@ -6,7 +6,7 @@ y <- suppressMessages(lapply(libs, require, character.only = TRUE))
 option_list <- list(
   make_option(c("-e", "--ECCs"), metavar = "file", default = "results/ECCs.tsv", help = "ECC result file"),
   make_option(c("-c", "--CGMs"), metavar = "file", default = "results/CGM_strain_results.txt", help = "CGM result file"),
-  make_option(c("-s", "--strains"), metavar = "file", default = "inputs/strain_data.tsv", help = "Strain metadata file"))
+  make_option(c("-s", "--strains"), metavar = "file", default = "inputs/strain_info.txt", help = "Strain metadata file"))
 
 arg <- parse_args(OptionParser(option_list=option_list))
 
@@ -39,20 +39,23 @@ step1[,grep("avg", ecccols, invert = TRUE, value = TRUE)] %<>% apply(., 2, repNA
 
 step1[,grep("tp1_id", colnames(step1))] %<>% apply(., 2, repNA, i = "") %>% as_tibble()
 
+c1 <- grep("TP1", ecccols, value = TRUE) %>% grep("avg", ., value = TRUE, invert = TRUE)
+c2 <- grep("TP2", ecccols, value = TRUE) %>% grep("avg", ., value = TRUE, invert = TRUE)
+
 step2 <- step1 %>% rename("TP1 cluster" = tp1_id) %>% 
   mutate("TP2 cluster" = first_tp2_flag, "TP1 cluster size" = tp1_cl_size, 
          "TP2 cluster size" = tp2_cl_size) %>% 
   select(Strain, Country, Province, City, Latitude, Longitude, Day, Month, Year, 
-         TP1, `TP1 cluster`, tp1_cl_size, grep("TP1", ecccols, value = TRUE), 
-         TP2, `TP2 cluster`, tp2_cl_size, grep("TP2", ecccols, value = TRUE), 
+         TP1, `TP1 cluster`, tp1_cl_size, all_of(c1), 
+         TP2, `TP2 cluster`, tp2_cl_size, all_of(c2), 
          first_tp1_flag, last_tp1_flag, first_tp2_flag, last_tp2_flag, `TP1 cluster size`, 
          `TP2 cluster size`, actual_size_change, add_TP1, num_novs, actual_growth_rate, new_growth) %>% 
   rename("TP1 cluster size (2)" = tp1_cl_size, 
          "TP2 cluster size (2)" = tp2_cl_size, 
-         "TP1 temporal cluster average (distances)" = TP1_avg_temp_ECC, 
-         "TP1 geo cluster average (distances)" = TP1_avg_geo_ECC, 
-         "TP2 temporal cluster average (distances)" = TP2_avg_temp_ECC, 
-         "TP2 geo cluster average (distances)" = TP2_avg_geo_ECC, 
+         # "TP1 temporal cluster average (distances)" = TP1_avg_temp_ECC, 
+         # "TP1 geo cluster average (distances)" = TP1_avg_geo_ECC, 
+         # "TP2 temporal cluster average (distances)" = TP2_avg_temp_ECC, 
+         # "TP2 geo cluster average (distances)" = TP2_avg_geo_ECC, 
          "First time this cluster was seen in TP1" = first_tp1_flag, 
          "Last time this cluster was seen in TP1" = last_tp1_flag, 
          "First time this cluster was seen in TP2" = first_tp2_flag, 
