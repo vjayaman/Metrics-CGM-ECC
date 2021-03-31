89ae0d5a11942b6d519679fec342db040650856e
vjayaman <vjayaman314@gmail.com>
2021-03-30 22:36:52 -0500

    Updated strain_results default to tsv instead of txt

--- merge_data.R
+++ merge_data.R
@@ -5,8 +5,9 @@ y <- suppressMessages(lapply(libs, require, character.only = TRUE))
 
 option_list <- list(
   make_option(c("-e", "--ECCs"), metavar = "file", default = "results/ECCs.tsv", help = "ECC result file"),
-  make_option(c("-c", "--CGMs"), metavar = "file", default = "results/CGM_strain_results.txt", help = "CGM result file"),
-  make_option(c("-s", "--strains"), metavar = "file", default = "inputs/strain_info.txt", help = "Strain metadata file"))
+  make_option(c("-c", "--CGMs"), metavar = "file", default = "results/CGM_strain_results.tsv", help = "CGM result file"),
+  make_option(c("-s", "--strains"), metavar = "file", default = "results/strain_info.txt", help = "Strain metadata file"))
+  # make_option(c("-s", "--strains"), metavar = "file", default = "inputs/strain_info.txt", help = "Strain metadata file"))
 
 arg <- parse_args(OptionParser(option_list=option_list))
 
