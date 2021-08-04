0357f00bbfadb4fb66bfb82cde242bd2970867c7
vjayaman <vjayaman314@gmail.com>
2021-08-04 16:38:33 -0500

    One more adjustment - will generate heatmaps unless number of clusters given is 0

--- run_process.sh
+++ run_process.sh
@@ -11,11 +11,11 @@ FORM=inputs/form_inputs.txt
 # DO NOT CHANGE pdata, it's a flag used to check for success
 pdata=0
 
-printf "\n\nPart 0/5:"
+printf "\n\nPart 0/6:"
 Rscript environment_setup.R
 
 
-printf "\n\nPart 1/5:"
+printf "\n\nPart 1/6:"
 if [ -f $RAWSTRAINS -a -f $TP1_CLUSTERS -a -f $TP2_CLUSTERS -a -f $FORM ]; then
 	Rscript scripts/1_prepare_inputs.R -m $RAWSTRAINS -a $TP1_CLUSTERS -b $TP2_CLUSTERS -d $FORM
 else
@@ -33,7 +33,7 @@ else
 fi
 
 
-printf "\n\n\nPart 2/5:"
+printf "\n\n\nPart 2/6:"
 if [ $pdata == 1 ]; then
 	Rscript scripts/2_cgm_collection.R -a $tp1_data -b $tp2_data -d $FORM
 else
@@ -41,7 +41,7 @@ else
 fi
 
 
-printf "\n\n\nPart 3/5:"
+printf "\n\n\nPart 3/6:"
 if [ $pdata == 1 ]; then
 	Rscript scripts/3_dist_matrices.R -m $STRAINS -a $tp1_data -b $tp2_data -d $FORM
 else
@@ -49,7 +49,7 @@ else
 fi
 
 
-printf "\n\n\nPart 4/5:"
+printf "\n\n\nPart 4/6:"
 if [ $pdata == 1 ]; then
 	Rscript scripts/4_ecc_collection.R -m $STRAINS -a $tp1_data -b $tp2_data -d $FORM
 else
@@ -57,14 +57,15 @@ else
 fi
 
 
-printf "\n\n\nPart 5/5:"
+printf "\n\n\nPart 5/6:"
 if [ -f "results/ECCs.tsv" -a -f "results/CGM_strain_results.tsv" -a -f $STRAINS ]; then
   Rscript scripts/5_combine_results.R -m $STRAINS -a inputs/processed/allTP1.Rds -b inputs/processed/allTP2.Rds -c results/CGM_strain_results.tsv -d $FORM -e results/ECCs.tsv
 else
   echo "Not all required files found for part 5."
 fi
 
-if [ -f "results/CGM_strain_results.tsv" -a -f "intermediate_data/TP2/dists/group01.Rds" -a -f $STRAINS ]; then
+printf "\n\n\nPart 6/6:"
+if [ -f "results/CGM_strain_results.tsv" -a -f "intermediate_data/dist_extremes.Rds" -a -f $STRAINS ]; then
 	Rscript scripts/6_heatmap_dists.R -m $STRAINS -a $tp1_data -b $tp2_data -c results/CGM_strain_results.tsv -d $FORM
 else
 	echo "Not all required files found for part 6."
