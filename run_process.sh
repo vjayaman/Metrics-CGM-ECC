a6faf5fd7ee509fb45ccb20bd7df3c2a47febfba
vjayaman <vjayaman314@gmail.com>
2021-08-13 10:16:38 -0500

    run_process.sh now brought up to match main

--- run_process.sh
+++ run_process.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# USER-SPECIFIED INPUTS, change these if necessary before running:
-
 # Strain metadata file, with "Strain" identifier column
 RAWSTRAINS=inputs/strain_info.txt
 
@@ -10,21 +8,14 @@ TP1_CLUSTERS=inputs/tp1_clusters_init.txt
 TP2_CLUSTERS=inputs/tp2_clusters_init.txt
 FORM=inputs/form_inputs.txt
 
-# Height of interest
-HEIGHT="0"
-
-# Two pairs of coefficient sets, each in order "source, temporal geographic". 
-# Example: "010" means source = 0, temporal = 1, geographic = 0
-PARAMS="0-1-0,0-0-1"
-
-# Do not change these variables unless necessary:
+# DO NOT CHANGE pdata, it's a flag used to check for success
 pdata=0
 
-printf "\n\nPart 0/5:"
+printf "\n\nPart 0/6:"
 Rscript environment_setup.R
 
 
-printf "\n\nPart 1/5:"
+printf "\n\nPart 1/6:"
 if [ -f $RAWSTRAINS -a -f $TP1_CLUSTERS -a -f $TP2_CLUSTERS -a -f $FORM ]; then
 	Rscript scripts/1_prepare_inputs.R -m $RAWSTRAINS -a $TP1_CLUSTERS -b $TP2_CLUSTERS -d $FORM
 else
@@ -42,39 +33,40 @@ else
 fi
 
 
-printf "\n\n\nPart 2/5:"
+printf "\n\n\nPart 2/6:"
 if [ $pdata == 1 ]; then
-	Rscript scripts/2_cgm_collection.R -a $tp1_data -b $tp2_data -x $HEIGHT
+	Rscript scripts/2_cgm_collection.R -a $tp1_data -b $tp2_data -d $FORM
 else
 	echo "Not all required files found for part 2."
 fi
 
 
-printf "\n\n\nPart 3/5:"
+printf "\n\n\nPart 3/6:"
 if [ $pdata == 1 ]; then
-	Rscript scripts/3_dist_matrices.R -m $STRAINS -a $tp1_data -b $tp2_data -x $HEIGHT
+	Rscript scripts/3_dist_matrices.R -m $STRAINS -a $tp1_data -b $tp2_data -d $FORM
 else
 	echo "Not all required files found for part 3."
 fi
 
 
-printf "\n\n\nPart 4/5:"
+printf "\n\n\nPart 4/6:"
 if [ $pdata == 1 ]; then
-	Rscript scripts/4_ecc_collection.R -m $STRAINS -a $tp1_data -b $tp2_data -x $HEIGHT -t $PARAMS
+	Rscript scripts/4_ecc_collection.R -m $STRAINS -a $tp1_data -b $tp2_data -d $FORM
 else
 	echo "Not all required files found for part 4."
 fi
 
 
-printf "\n\n\nPart 5/5:"
+printf "\n\n\nPart 5/6:"
 if [ -f "results/ECCs.tsv" -a -f "results/CGM_strain_results.tsv" -a -f $STRAINS ]; then
   Rscript scripts/5_combine_results.R -m $STRAINS -a inputs/processed/allTP1.Rds -b inputs/processed/allTP2.Rds -c results/CGM_strain_results.tsv -d $FORM -e results/ECCs.tsv
 else
   echo "Not all required files found for part 5."
 fi
 
-if [ -f "results/CGM_strain_results.tsv" -a -f "intermediate_data/TP2/dists/group1.Rds" -a -f $STRAINS ]; then
-	Rscript scripts/6_heatmap_dists.R -m $STRAINS -a $tp1_data -b $tp2_data -c results/CGM_strain_results.tsv -t 0.0 -g 1.0 -l 5 -x $HEIGHT
+printf "\n\n\nPart 6/6:"
+if [ -f "results/CGM_strain_results.tsv" -a -f "intermediate_data/dist_extremes.Rds" -a -f $STRAINS ]; then
+	Rscript scripts/6_heatmap_dists.R -m $STRAINS -a $tp1_data -b $tp2_data -c results/CGM_strain_results.tsv -d $FORM
 else
 	echo "Not all required files found for part 6."
 fi
