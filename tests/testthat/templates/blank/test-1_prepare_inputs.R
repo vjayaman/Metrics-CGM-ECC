1b846881f092b8a437d2483f3c055a2a352770e3
vjayaman <vjayaman314@gmail.com>
2022-02-14 14:41:51 -0600

    Tentatively fixed week bug, added T0.original to processed/allTP2.Rds file

--- tests/testthat/templates/blank/test-1_prepare_inputs.R
+++ tests/testthat/templates/blank/test-1_prepare_inputs.R
@@ -3,136 +3,152 @@ library(testthat)
 y <- suppressPackageStartupMessages(lapply(libs, require, character.only = TRUE))
 assert("All packages loaded correctly", all(unlist(y)))
 
-source("scripts/Misc/formatprep.R")
+# source("scripts/Misc/formatprep.R")
 
 test_results <- vector(length = 12) %>% 
   setNames(c("Part 1", "Part 2", "Part 3", "Part 4", "Part 5", "Part 6", 
-             "Part 7", "Part 8", "Part 9", "Part 10", "Part 11", "Part 12", "Part 13"))
-
-# option_list <- list(
-#   make_option(c("-m", "--metadata"), metavar = "file",
-#               default = "inputs/strain_info.txt", help = "Metadata file"),
-#   make_option(c("-b", "--tp2"), metavar = "file",
-#               default = "inputs/tp2_clusters_init.txt", help = "TP2 cluster assignments"),
-#   make_option(c("-d", "--details"), metavar = "file",
-#               default = "inputs/form_inputs.txt", help = "Analysis inputs (details)"),
-#   make_option(c("-p", "--pangolineages"), metavar = "file",
-#               default = "inputs/GISAID Lineages (770000 isolates).csv",
-#               help = "File with Strain column and Pango_lineage column"))
-# 
-# arg <- parse_args(OptionParser(option_list=option_list)); rm(option_list)
+             "Part 7", "Part 8", "Part 9", "Part 10", "Part 11", "Part 12"))
 
 # FUNCTIONS ----------------------------------------------------------------------------------------------------
+checkEncoding <- function(fp) {
+  readr::guess_encoding(fp) %>% arrange(-confidence) %>% slice(1) %>% pull(encoding) %>% return()
+}
+
+# function for reading raw strains and time point clusters
+readData <- function(path, check_enc = TRUE) {
+  if (check_enc) {
+    enc <- checkEncoding(file.path(path))
+  }else {
+    enc <- ""
+  }
+  
+  file.path(path) %>% 
+    read.table(sep="\t", header=TRUE, fileEncoding=enc, fill=TRUE, quote="") %>% 
+    as_tibble() %>% return()
+}
+
+# START TESTING --------------------------------------------------------------------------------------------
+init_inputs <- c("form_inputs.txt", "tp2_clusters_init.txt", "strain_info.txt")
+post_inputs <- c("allTP2.Rds", "strain_info.txt")
 
 # Part 1
-# params <- readLines(arg$details, warn = FALSE) %>% strsplit(., split = ": ") %>% 
-#   set_names(c("reg","cou","has_lin", "has_date","has_prov","prov",
-#               "th","nsTP1","nsTP2","temp_win","cnames"))
-# a1 <- readData(arg$metadata, FALSE)
-# a2 <- suppressWarnings(readData(arg$metadata, check_enc = TRUE))
-# if (nrow(a1) > nrow(a2)) {strain_data <- a1}else {strain_data <- a2}
-# time1 <- suppressWarnings(readData(arg$tp1, check_enc = TRUE))
-# if (!exists("time1")) {time1 <- readData(arg$tp1, FALSE)}
-# time2 <- suppressWarnings(readData(arg$tp2, check_enc = TRUE))
-# if (!exists("time2")) {time2 <- readData(arg$tp2, FALSE)}
-test_that("Part 1", {})
+# required files exist and 1_prepare_inputs.rscript has been run successfully
+test_that("Part 1", {
+  inps_pre_script1 <- init_inputs %in% list.files("inputs")
+  
+  expect_true(all(inps_pre_script1))
+  expect_true(dir.exists("inputs/processed"))
+  
+  inps_post_script1 <- post_inputs %in% list.files("inputs/processed/")
+
+  expect_true(all(inps_post_script1))
+})
+
+
+test_that("Part 2", {
+  a1 <- readData(file.path("inputs", init_inputs[3]))
+  b1 <- c("Source", "Country", "Province", "City",
+            "Day", "Month", "Year") %in% colnames(a1)
+  b2 <- a1 %>% pull(Day) %>% unique() %>% sort() %>% as.integer()
+  b3 <- a1 %>% pull(Month) %>% unique() %in% 1:12 %>% all()
+
+  # required columns found
+  expect_true(all(b1))
+  # day columns are in [1,31]
+  expect_true(all(b2 %in% 1:31))
+  # month columns are in [1,12]
+  expect_true(all(b3 %in% 1:12))
+})
+
+# check that updateStrains was done correctly, processed input files should have 
+# the same sets of strains
+strain_data <- read_tsv(file.path("inputs/processed", post_inputs[2]))
+tp2_data <- readRDS(file.path("inputs/processed", post_inputs[1]))
+
+
+# Check that dates were processed correctly
+test_that("Part 3", {
+  col1 <- c("Strain", "Latitude", "Longitude", "Date", "YearMonth", "YearWeek") %in% colnames(strain_data)
+  expect_true(all(col1))
+  
+  x1 <- strain_data %>% pull(Date)
+  x2 <- strain_data %>% pull(YearMonth)
+  lapply(1:nrow(strain_data), function(i) {
+    substr(x1[i], 1, 7) %>% identical(., x2[i])
+    }) %>% unlist() %>% all() %>% expect_true()
+
+  x3 <- strain_data %>% pull(YearWeek)
+  weeks <- strain_data %>% pull(Date) %>% strftime(., format = "%Y-%V")
+  expect_identical(x3, weeks)
+})
+
+# check that new_cols, lookup, and original are correct
+test_that("Part 4a", {
+  expect_identical(c("lookup_table", "new_cols", "original", "strain_pango", "pango_clusters"), names(tp2_data))
+  
+  lookup <- tp2_data$lookup_table %>% arrange(old_h, old_cl) %>% mutate(across(new_h, as.character))
+  
+  tp2_raw <- read_tsv(file.path("inputs", init_inputs[2]))
+  tp2_raw %>% as.data.table() %>% melt.data.table(id.vars = "Strain") %>% select(-Strain) %>% unique()
+  
+  part_new <- tp2_data$new_cols %>% as.data.table() %>% melt.data.table(id.vars = "Strain") %>% 
+    set_colnames(c("Strain", "new_h", "new_cl")) %>% mutate(across(new_h, as.character)) %>% 
+    mutate(across(new_h, as.integer))
+  
+  part_old <- tp2_data$original %>% as.data.table() %>% melt.data.table(id.vars = "Strain") %>% 
+    set_colnames(c("Strain", "old_h", "old_cl"))
+  
+  old_h_vals <- part_old$old_h %>% unique() %>% sort()
+  df <- data.table(old_h = old_h_vals, new_h = 0:(length(old_h_vals)-1))
+  
+  new_lookup <- part_old %>% 
+    merge.data.table(., df, by = "old_h") %>% 
+    merge.data.table(., part_new, by = c("Strain", "new_h")) %>% 
+    select(old_h, old_cl, new_h, new_cl) %>% unique() %>% 
+    arrange(old_h, old_cl) %>% 
+    mutate(across(old_h, as.character)) %>% 
+    mutate(across(new_h, as.character))
+    
+  expect_equal(new_lookup, lookup)
+})
+
+pango_lineages <- read.csv("inputs/GISAID Lineages (770000 isolates).csv") %>% 
+  select(Strain, T0.original, Pango_lineage) %>% 
+  as.data.table()
+
+test_that("Part 4d", {
+  tp2_data$strain_pango %>% as.data.table() %>% 
+    expect_equal(., pango_lineages)
+})
+
+test_that("Part 4e", {
+  
+  df <- tp2_data$new_cols %>% 
+    melt.data.table(id.vars = "Strain") %>% 
+    set_colnames(c("Strain", "new_h", "new_cl"))
+  
+  lookup <- tp2_data$lookup_table %>% arrange(old_h, old_cl) %>% mutate(across(new_h, as.character))
+  
+  actual <- tp2_data$pango_clusters %>% 
+    mutate(across(new_h, as.integer))
+  
+  new_df <- tp2_data$strain_pango %>% 
+    merge.data.table(., df, by = "Strain") %>% 
+    merge.data.table(., lookup, by = c("new_h", "new_cl")) %>% 
+    select(colnames(tp2_data$pango_clusters))
+    
+  expect_equal(new_df, actual)
+    
+  
+  
+})
+
+# strain_pango
+# pango_clusters
 
 # Part 2
-# initial_sizes <- tibble(type="initial", a=nrow(strain_data), b=nrow(time1), d=nrow(time2))
-# assert("Has lineage info", as.logical(params$has_lin[2]))
-# x <- updateStrains("lin_info", strain_data, time1, time2, initial_sizes)
-# strain_data <- x$sd; time1 <- x$t1; time2 <- x$t2; initial_sizes <- x$sizes
 test_that("Part 2", {})
 
-# Part 3
-# reqnames <- c("Strain", "Latitude", "Longitude", "Day", "Month", "Year")
-# cnames <- params$cnames[2] %>% strsplit(split = ",") %>% unlist()
-# if (!("none" %in% cnames)) {
-#   fullcnames <- c(reqnames, cnames)
-# }else {
-#   fullcnames <- reqnames
-# }
-# strain_data <- strain_data %>% select(all_of(fullcnames)) %>% 
-#   na.omit(Strain) %>% na.omit(Latitude) %>% na.omit(Longitude) %>% na.omit(Day) %>% 
-#   na.omit(Month) %>% na.omit(Year)
-test_that("Part 3", {})
-
-# Part 4
-# # add column to show which strains are found in TP1
-# strain_data %<>% mutate(TP1 = ifelse(Strain %in% time1$Strain, 1, 0))
-# # add column to show which strains are found in TP2
-# strain_data %<>% mutate(TP2 = ifelse(Strain %in% time2$Strain, 1, 0))
-test_that("Part 4", {})
-
-# Part 5
-# if (params$reg[2] != "All" & "Region" %in% colnames(strain_data)) {
-#   strain_data <- strain_data %>% filter(Region %in% params$reg[2])
-# }
-test_that("Part 5", {})
-
-# Part 6
-# if (params$cou[2] != "All" & "Country" %in% colnames(strain_data)) {
-#   strain_data <- strain_data %>% filter(Country %in% params$cou[2])
-# }
-test_that("Part 6", {})
-
-# Part 7
-# if (as.logical(params$has_date[2])) {
-#   strain_data <- strain_data %>% 
-#     filter(!is.na(Day)) %>% filter(!is.na(Month)) %>% filter(!is.na(Year)) %>% 
-#     filter(Day != "") %>% filter(Month != "") %>% filter(Year != "")
-# }
-test_that("Part 7", {})
-
-# Part 8
-# if (nchar(params$temp_win[2]) > nchar("[,]")) {
-#   tempwindow <- params$temp_win[2] %>% gsub("\\[|\\]", "", .) %>% 
-#     strsplit(., ",") %>% unlist()
-#   strain_data %<>% mutate(Date = as.Date(paste(Year, Month, Day, sep = "-"))) %>% 
-#     filter(Date >= tempwindow[1] & Date <= tempwindow[2])
-#   # Strains with metadata and defined lineage info at TP1
-#   y <- updateStrains("temp_win", strain_data, time1, time2, initial_sizes)
-#   strain_data <- y$sd; time1 <- y$t1; time2 <- y$t2; initial_sizes <- y$sizes
-# }
-test_that("Part 8", {})
-
-# Part 9
-# if (as.logical(params$has_prov[2])) {
-#   strain_data <- strain_data %>% filter(!is.na(Province)) %>% filter(Province != "")
-#   if (params$prov[2] != "All") {
-#     strain_data <- strain_data %>% filter(Province %in% params$prov[2])
-#   }
-#   # Strains with metadata and defined lineage info at TP2
-#   z <- updateStrains("aft_prov", strain_data, time1, time2, initial_sizes)
-#   strain_data <- z$sd; time1 <- z$t1; time2 <- z$t2; initial_sizes <- z$sizes
-# }
-test_that("Part 9", {})
-
-# Part 10
-# processed_tp1 <- intClusters(time1)
-# processed_tp2 <- intClusters(time2)
-test_that("Part 10", {})
-
-# Part 11
-# th <- params$th[2]
-# if (as.logical(params$nsTP1[2])) {
-#   remove_strains <- strainsInSingletons(processed_tp1$new_cols, th)
-#   processed_tp1$new_cols %<>% filter(!(Strain %in% remove_strains))
-#   processed_tp2$new_cols %<>% filter(!(Strain %in% remove_strains))
-#   strain_data %<>% filter(!(Strain %in% remove_strains))
-#   initial_sizes %<>% add_row(tibble(type="tp1_ns", a=nrow(strain_data), b=nrow(time1), d=nrow(time2)))
-# }
-test_that("Part 11", {})
-
-# Part 12
-# if (as.logical(params$nsTP2[2])) {
-#   remove_strains <- strainsInSingletons(processed_tp2$new_cols, th)
-#   processed_tp1$new_cols %<>% filter(!(Strain %in% remove_strains))
-#   processed_tp2$new_cols %<>% filter(!(Strain %in% remove_strains))
-#   strain_data %<>% filter(!(Strain %in% remove_strains))
-#   initial_sizes %<>% add_row(tibble(type="tp2_ns", a=nrow(strain_data), b=nrow(time1), d=nrow(time2)))
-# }
-test_that("Part 12", {})
 
 # Part 13
 # writeData(strain_data, file.path(arg$inputdir, "processed", "strain_info.txt"))
