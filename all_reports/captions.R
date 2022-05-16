d9b6f653b2167c6c986e42f152644ff8c07bac98
vjayaman <vjayaman314@gmail.com>
2022-05-16 13:23:18 -0500

    Verifying some outputs of metrics gen; streamlining report

---
+++ all_reports/captions.R
@@ -0,0 +1,90 @@
+## Create figure caption variables 
+Fig.1.1.A <- "Geospatial and temporal ECC distribution at TP1 and TP2 (3D surface density)"
+Fig.1.1.B <- "Geospatial and temporal ECC distribution at TP1 and TP2 (histogram)"
+Fig.1.1.C <- "Geospatial and temporal ECC distribution at TP1 and TP2 (Bubble plot)"
+
+Fig.1.2.A <- "Bubble plots with directionality vectors"
+Fig.1.2.B <- "Summary of directionality vectors by TP1 cluster size"
+Fig.1.2.C <- "Summary of directionality vectors by cluster growth rate"
+Tabble.1.1<- "ECC change vector classes and possible transmission modes"
+Fig.1.2.D <- "Summary of directionality vectors by ECC and Delta ECC level"
+
+Fig.1.3.A <- "Geographical spread over time (by country and province)"
+Fig.1.3.B <- "Lineage prevalence over time (top 20 clusters and others)"
+Fig.1.3.C <- "Growth rate distribution by TP, month and week"
+Fig.1.3.D <- "Lineage and genome distribution by growth rate (by TP)"
+Fig.1.3.E <- "Distribution of pairwise geospatial and temporal distances (TP1 and TP2)"
+Fig.1.3.F <- "Characteristics of top 20 clusters (by TP2 size)"
+Fig.1.3.G <- "Geospatial and temporal centroids for top 20 clusters (by TP2 size)"
+Fig.1.3.H <- "Characteristics of top 20 clusters (by TP2 growth rate)"
+Fig.1.3.I <- "Geospatial and temporal centroids for top 20 clusters (TP2 growth rate)"
+
+
+Fig.1.4.A <- "Monthly Geo and Temp ECC (average)"
+Fig.1.4.B <- "Top 10 clusters (by TP2 size) Geo ECC, Geo-Temp ECC, Temp ECC"
+Fig.1.4.C <- "Top 10 clusters (by TP2 growth rate) Geo ECC, Geo-Temp ECC, Temp ECC"
+
+Fig.1.5.A <- "Weekly Geo and Temp ECC (average)"
+Fig.1.5.B <- "Top 10 clusters (by TP2 size) Geo ECC, Geo-Temp ECC, Temp ECC"
+Fig.1.5.C <- "Top 10 clusters (by TP2 growth rate) Geo ECC, Geo-Temp ECC, Temp ECC"
+
+Fig.2.1.A <- "GEO + 50-50 + TEMP heatmaps for top 5 cluster (by TP2 size)"
+Fig.2.1.B <- "GEO + 50-50 + TEMP heatmaps for top 4 cluster (by TP2 size)"
+Fig.2.1.C <- "GEO + 50-50 + TEMP heatmaps for top 3 cluster (by TP2 size)"
+Fig.2.1.D <- "GEO + 50-50 + TEMP heatmaps for top 2 cluster (by TP2 size) "
+Fig.2.1.E <- "GEO + 50-50 + TEMP heatmaps for top 1 cluster (by TP2 size)"
+
+Fig.2.1.F <- "GEO + 50-50 + TEMP frequency for top 5 cluster (by TP2 size)"
+Fig.2.1.G <- "GEO + 50-50 + TEMP frequency for top 4 cluster (by TP2 size)"
+Fig.2.1.H <- "GEO + 50-50 + TEMP frequency for top 3 cluster (by TP2 size)"
+Fig.2.1.I <- "GEO + 50-50 + TEMP frequency for top 2 cluster (by TP2 size)"
+Fig.2.1.J <- "GEO + 50-50 + TEMP frequency for top 1 cluster (by TP2 size)"
+
+Fig.2.2.A <- "GEO + 50-50 + TEMP heatmaps for top 5 cluster (by TP2 growth rate)"
+Fig.2.2.B <- "GEO + 50-50 + TEMP heatmaps for top 4 cluster (by TP2 growth rate)"
+Fig.2.2.C <- "GEO + 50-50 + TEMP heatmaps for top 3 cluster (by TP2 growth rate)"
+Fig.2.2.D <- "GEO + 50-50 + TEMP heatmaps for top 2 cluster (by TP2 growth rate)"
+Fig.2.2.E <- "GEO + 50-50 + TEMP heatmaps for top 1 cluster (by TP2 growth rate)"
+
+Fig.2.2.F <- "GEO + 50-50 + TEMP pairwise distance frequency for top 5 cluster (by TP2 growth rate)"
+Fig.2.2.G <- "GEO + 50-50 + TEMP pairwise distance frequency for top 4 cluster (by TP2 growth rate)"
+Fig.2.2.H <- "GEO + 50-50 + TEMP pairwise distance frequency for top 3 cluster (by TP2 growth rate)"
+Fig.2.2.I <- "GEO + 50-50 + TEMP pairwise distance frequency for top 2 cluster (by TP2 growth rate)"
+Fig.2.2.J <- "GEO + 50-50 + TEMP pairwise distance frequency for top 1 cluster (by TP2 growth rate)"
+
+Fig.2.3.A <- "Distribution of pairwise distances for top 5 clusters (by TP2 size)"
+Fig.2.3.B <- "Distribution of pairwise distances for top 5 clusters (by TP2 growth rate)"
+
+Tab.3.1.a <- "SARS-Cov2 genomes by country and TP/months"
+Tab.3.1.b <- "SARS-Cov2 genomes by country and week"
+
+Tab.3.2.a <- "SARS-Cov2 genomes by PANGO lineages and country"
+Tab.3.2.b <- "SARS-Cov2 genomes by PANGO lineages and TP/month"
+Tab.3.2.c <- "SARS-Cov2 genomes by PANGO lineages and week"
+Tab.3.2.d <- "Change vector directionality"
+
+Fig.4.1.A.I <-  "TP1 vs TP2 genome counts"
+Fig.4.1.A.II <- "Cumulative genome counts by month, faceted by country"
+Fig.4.1.A.III <-"Genome counts, faceted by month"
+Fig.4.1.A.IV <- "Cumulative genome counts by week, faceted by country "
+Fig.4.1.A.V  <- "Genome counts, faceted by week "
+
+Fig.4.1.B.I  <-  "TP1 vs TP2 genome counts"
+Fig.4.1.B.II <- "Cumulative genome counts by month, faceted by country"
+Fig.4.1.B.III <-"Genome counts, faceted by month"
+Fig.4.1.B.IV <- "Cumulative genome counts by week, faceted by country "
+Fig.4.1.B.V  <- "Genome counts, faceted by week "
+
+Fig.4.2.A.I <- "Predominant lineages by genome count (top20)"
+Fig.4.2.B.I <-  "Cumulative genome counts for most prevalent lineages, by country"
+Fig.4.2.C.I <-  "Cumulative genome counts for most prevalent lineages, by country"
+
+Fig.4.3.A.I <-  "TP1 vs TP2  of countries detected "
+Fig.4.3.A.II <- "Countries detected: most prevalent lineages, by month"
+Fig.4.3.A.III <- "Countries detected: most prevalent lineages, by week"
+
+Fig.4.3.B.I <-  "TP1 vs TP2 of	Provinces detected"
+Fig.4.3.B.II <- "Provinces detected: most prevalent lineages, by month"
+Fig.4.3.B.III <- "Provinces detected: most prevalent lineages, by week"
+
+Fig.4.4 <- "ECC direction classes vs compass rose"
